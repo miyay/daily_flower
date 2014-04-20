b35ac076a8553cce67a6622733183ec34cf142be
miyay <yuuma_117@hotmail.com>
2014-04-20 14:42:51 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DailyFlower::Application.config.secret_token = '5f28c2554a4518d66ac11b9d620a4f9597b7c51e0150ff82cfe5410e985efdb29c0554effbe60a8f5444117c5ebe36cd976aea7d2497ba7bf19c068ec7d5e061'
